f6d553cf6307662354d58405c4b6855e1f3afc17
Jacktrooper <jack.corso@flemingcollege.ca>
2023-02-22 17:45:38 -0500

    final product of lab 5 Poke API

--- main_script_api.py
+++ main_script_api.py
@@ -7,14 +7,14 @@ def main():
     poke_list = search_for_pokemon(search_term)
     if poke_list:
         title, body_text = get_paste_data(poke_list, search_term)
-        paste_url = post_new_paste(title, body_text)
+        new_paste_url = new_paste(title, body_text)
+        print(f'If needed URL od new past: {new_paste_url}')
         
 
     return
 
 def get_search_term():
     num_params = len(sys.argv) - 1
-    print(f'Posting new paste to pastebin...', end='')
     if num_params > 0:
         return sys.argv[1]
     else:
@@ -22,13 +22,18 @@ def get_search_term():
         sys.exit(1)
 
 def get_paste_data(poke_list, search_term):
-    
-    title = f'The pokemons number or name is: "{search_term}"'
-    
+    poke_name = poke_list['name'].lower()
+    poke_abilities = ['- ' + ability['ability']['name'] for ability in poke_list['abilities']]
+    title = f'The pokemons name is: "{poke_name}"'
     divider = '\n'
     body_text = divider.join(poke_list)
 
     return title, body_text
+
+def new_paste(title, body_text):
+    new_paste_url = post_new_paste(title, body_text, '1M', '0')
+    
+    return new_paste_url
 
 if __name__ == '__main__':
     main()
